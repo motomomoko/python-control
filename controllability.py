384b850c3c1dee156a235c4e1ac5ff4509e1d4d2
momoko nishimoto <24motomomoko@gmail.com>
2017-03-13 19:06:55 +0900

    可制御性の判定

---
+++ controllability.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from control.matlab import *
+import numpy as np
+
+def main():
+  # システム行列の定義
+    A = "0 1; -1 -1"
+    B = "0; 1"
+    C= "1 0"
+    D = "0"
+    # 状態空間モデルの作成
+    sys = ss(A, B, C, D)
+    Uc = ctrb(A, B) # 可制御性行列の計算
+    Nu = np.linalg.matrix_rank(Uc)  # Ucのランクを計算
+    (N, N) = np.matrix(A).shape     # 正方行列Aのサイズ(N*N)
+    # 可制御性の判別
+    if Nu == N: 
+        print("システムは可制御である") 
+    else: 
+		print("システムは可制御でない") 
+
+										    
+if __name__ == "__main__":
+    main()
